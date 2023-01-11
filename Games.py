2ca55ad26cfbac4dbba8e20f513e9ca703b5bf81
ofir29 <120117571+ofir29@users.noreply.github.com>
2023-01-11 20:41:26 +0200

    Delete Games.py

--- Games.py
+++
@@ -1,9 +0,0 @@
-def boom(x, boom_num):
-    numbers = range(1, x)
-    for number in numbers:
-        if number % boom_num == 0 or str(boom_num) in str(number):
-            print("BOOM!")
-        else:
-            print(number)
-
-boom(100, 7)
